c48a94ec73ca7f6b20b97b24b612fd41a1df52ba
ayyansinan <alexmunir2008@gmail.com>
2024-06-10 19:56:48 -0400

    Update laser.lua

--- laser.lua
+++ laser.lua
@@ -1,1 +1,13 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+local laser = game.Workspace.laser
+
+local raycastparms = Raycastparams.new()
+
+raycastparams.FilterDescendadentsInstance = {laser}
+
+local result = game.Workspace:Raycast(laser.Part.Position, laser.Part.Position + Vector3.new(40, 0, 0), raycastparams)
+
+if result then
+  local target = result.Instance
+end

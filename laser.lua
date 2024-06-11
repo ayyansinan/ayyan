932aa34af1d42c791960c5edc91374ef630197e1
ayyan <alexmunir2008@gmail.com>
2024-06-11 16:55:47 -0400

    Update laser.lua

--- laser.lua
+++ laser.lua
@@ -3,13 +3,14 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 -- a variable referencing to the laser model in the workspace
 local laser = game.Workspace.laser
 
-
 local raycastparms = Raycastparams.new()
 -- filters the descendants of the Instance the raycast has hit if the Instance the raycast has hit is the Instance in this table
 raycastparams.FilterDescendadentsInstance = {laser}
 
+local direction = (laser.Position)
+
 -- making a raycasting giving 2 arguments the position where the ray should start and the position where the ray should end then the last argument is the params
-local result = game.Workspace:Raycast(laser.Part.Position, laser.Part.Position + Vector3.new(40, 0, 0), raycastparams)
+local result = game.Workspace:Raycast(laser.Part.Position, raycastparams)
 
 local debounce = true
 

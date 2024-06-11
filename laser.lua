9907cb8d9564f5e24ea92e3333e3ab7bcacabc51
ayyansinan <alexmunir2008@gmail.com>
2024-06-10 20:03:25 -0400

    Update laser.lua

--- laser.lua
+++ laser.lua
@@ -8,6 +8,13 @@ raycastparams.FilterDescendadentsInstance = {laser}
 
 local result = game.Workspace:Raycast(laser.Part.Position, laser.Part.Position + Vector3.new(40, 0, 0), raycastparams)
 
+local debounce = true
+
 if result then
   local target = result.Instance
+  if target.Parent:FindFirstChild("Humanoid") and debounce == true then
+    debounce = false
+    target.Parent.Humanoid.Health -= 10
+    wait(1)
+    debounce = true
 end
